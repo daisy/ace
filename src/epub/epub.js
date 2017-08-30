880c12c3e69d2740dbd0cbce145b33b7bc25c04c
Marisa DeMeglio <marisa.demeglio@gmail.com>
2017-08-30 16:49:30 -0700

    can run on unpacked epub
    fixes #32

--- src/epub/epub.js
+++ src/epub/epub.js
@@ -3,6 +3,8 @@
 const epubParse = require('./epub-parse.js');
 const unzip = require('extract-zip');
 const tmp = require('tmp');
+const fs = require('fs-extra');
+const path = require('path');
 
 function EPUB(path) {
   this.path = path;
@@ -10,22 +12,39 @@ function EPUB(path) {
   this.navDoc = {};
 }
 
-EPUB.prototype.extract = function extract() {
-  tmp.setGracefulCleanup(); // remove tmpdir automatically upon process exit
-  const tmpdir = tmp.dirSync({ unsafeCleanup: true }); // remove even when not empty
-  console.log('Unzipping...');
+EPUB.prototype.isUnpacked = function isUnpacked() {
+  // assuming this fileset already passed epubcheck
+  // so no need to verify its format
+  return fs.statSync(this.path).isDirectory();
+};
 
-  return new Promise((resolve, reject) => {
-    unzip(this.path, { dir: tmpdir.name }, (err) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      this.dir = tmpdir.name;
-      // console.log(`Unzipped EPUB.`);
+EPUB.prototype.extract = function extract() {
+
+  // only extract if not already unpacked
+  if (this.isUnpacked()) {
+    this.dir = this.path;
+    return new Promise((resolve, reject) => {
       resolve(this);
     });
-  });
+  }
+
+  else {
+    tmp.setGracefulCleanup(); // remove tmpdir automatically upon process exit
+    const tmpdir = tmp.dirSync({ unsafeCleanup: true }); // remove even when not empty
+    console.log('Unzipping...');
+
+    return new Promise((resolve, reject) => {
+      unzip(this.path, { dir: tmpdir.name }, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        this.dir = tmpdir.name;
+        // console.log(`Unzipped EPUB.`);
+        resolve(this);
+      });
+    });
+  }
 };
 
 EPUB.prototype.parse = function parse() {
