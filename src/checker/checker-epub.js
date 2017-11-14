16fbca8e40878c1b8090eb829faa2b963c151127
Lars Voigt <lars.voigt@dzb.de>
2017-11-14 17:21:19 +0100

    - refactoring:  - use rule as primary terminology instead of check - a rule contains the description for an audit  - make rule check reference relative to rule definition

--- src/checker/checker-epub.js
+++ src/checker/checker-epub.js
@@ -1,28 +1,52 @@
 'use strict';
 
 const builders = require('../report/report-builders.js');
+
 const winston = require('winston');
 const glob = require('glob');
+const path = require('path');
 
 const ASSERTED_BY = 'Ace';
 const MODE = 'automatic';
 const KB_BASE = 'http://kb.daisy.org/publishing/';
+const RULE_BASE_PATH = './src/checker';
 
-const checks = [];
+const rules = [];
+const ruleProperties = ['title', 'testDesc', 'kbTitle', 'resDesc', 'kbPath', 'impact', 'tag', 'spec', 'check'];
 
-function validateCheck() {
-  // TODO: impl check validator
+function isRuleFormatValid(rule) {
+  if (Object.keys(rule).length !== ruleProperties.length) {
+    winston.error(`Rule format not valid: \n${JSON.stringify(rule)}`);
+    return false;
+  }
+  ruleProperties.forEach((prop) => {
+    if (!Object.prototype.hasOwnProperty.call(rule, prop)) {
+      winston.error(`Property ${prop} not found! \n${JSON.stringify(rule)}`);
+      return false;
+    }
+    return true;
+  });
+
+  if (rule.check.toString().length === 0) {
+    winston.error(`Rule-property 'check' should never be empty. \n${JSON.stringify(rule)}`);
+    return false;
+  }
   return true;
 }
 
-// use glob patterns to parse checks
-const files = glob.sync('./**/*.json', { cwd: './src/checker' });
+// use glob patterns to parse rules
+const files = glob.sync('./**/*.json', { cwd: RULE_BASE_PATH });
 files.forEach((file) => {
-  const checkOject = require(file);
-  if (validateCheck(checkOject)) { checks.push(checkOject); }
+  const rule = require(file);
+  if (isRuleFormatValid(rule)) {
+    rule.check = path.resolve(RULE_BASE_PATH, path.dirname(file), rule.check);
+    rules.push(rule);
+  }/* else TODO: Format of any rule is not valid. Stop the whole processing?
+       process.exit(1);
+  */
 });
 
-// console.log(JSON.stringify(checks));
+// console.log(JSON.stringify(rules));
 
 
 function asString(arrayOrString) {
@@ -57,21 +81,27 @@ function newViolation({ impact = 'serious', title, testDesc, resDesc, kbPath, kb
 
 function check(epub, report) {
   winston.info('Checking package...');
+
+  // normalize title property for future processing
+  // TODO: Can asString() replaced by (epub.metadata['dc:title'] || '').toString().trim()
+  // TODO: or it is possible that metadata['dc:title'] include more than one element?
+  epub.metadata['dc:title'] = asString(epub.metadata['dc:title']);
+
   const assertion = new builders.AssertionBuilder()
     .withSubAssertions()
-    .withTestSubject(epub.packageDoc.src, asString(epub.metadata['dc:title']));
+    .withTestSubject(epub.packageDoc.src, epub.metadata['dc:title']);
 
-  // run all "checks"
-  checks.forEach((checkObject) => {
-    const run = require(checkObject.audit);
-    if (run(epub)) { assertion.withAssertions(newViolation(checkObject)); }
+  // check all rules
+  rules.forEach((rule) => {
+    const runCheck = require(rule.check);
+    if (runCheck(epub)) {
+      assertion.withAssertions(newViolation(rule));
+    }
   });
 
   report.addAssertions(assertion.build());
-
   // Report the Nav Doc
   report.addEPUBNav(epub.navDoc);
-
   // Report package properties
   report.addProperties({
     hasManifestFallbacks: epub.hasManifestFallbacks,
