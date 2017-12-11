437d9133a05eefaa7a8eac8d520d230b301525a4
Lars Voigt <lars.voigt@dzb.de>
2017-12-11 15:59:51 +0100

    - delete dist folder

--- dist/checker/rules/metadata/schema-accessMode.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-// check
-
-module.exports = epub => epub.metadata['schema:accessMode'] === undefined;
--- dist/checker/rules/metadata/source.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-// check
-
-module.exports = epub => epub.navDoc.hasPageList && (epub.metadata['dc:source'] === undefined || epub.metadata['dc:source'].toString() === '');
