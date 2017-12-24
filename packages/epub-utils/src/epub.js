4c8b5326cc515f43bfb6a656116cbb7a13aacc8a
Romain Deltour <rdeltour@gmail.com>
2017-12-24 03:12:40 +0100

    fix(unzip): try to repair ZIP archives with extra comment length (#138)
    Fixes #121

--- packages/epub-utils/src/epub.js
+++ packages/epub-utils/src/epub.js
@@ -1,13 +1,49 @@
 'use strict';
 
 const epubParse = require('./epub-parse.js');
-const unzip = require('extract-zip');
+const extractZip = require('extract-zip');
 const tmp = require('tmp');
 const fs = require('fs-extra');
 const path = require('path');
 const winston = require('winston');
 
 tmp.setGracefulCleanup();
+
+async function unzip(path) {
+  const tmpdir = tmp.dirSync({ unsafeCleanup: true }).name;
+  return new Promise((resolve, reject) => {
+    extractZip(path, { dir: tmpdir }, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(tmpdir);
+      }
+    });
+  }) 
+}
+
+async function retryUnzip(epub, error) {
+  if (error.message === undefined) throw error;
+  winston.info('Trying to repair the archive and unzip again...');
+  try {
+    // Detect 'invalid comment length' errors
+    const invalidCommentLengthMatch =  error.message.match(/invalid comment length\. expected: (\d+)\. found: (\d)/);
+    if (invalidCommentLengthMatch) {
+      const tmpEPUB = tmp.fileSync({ unsafeCleanup: true }).name;
+      const size  = fs.statSync(epub.path).size;
+      const truncatedSize = size - invalidCommentLengthMatch[1];
+      fs.copySync(epub.path, tmpEPUB);
+      fs.truncateSync(tmpEPUB, truncatedSize);
+      return await unzip(tmpEPUB);
+    } else {
+      winston.error('The ZIP archive couldn’t be repaired.');
+    }
+  } catch (error) {
+    winston.error('Unzipping failed again');
+    winston.debug(error);
+  }
+  throw error;
+}
 
 class EPUB {
   constructor(epub, cwd = process.cwd()) {
@@ -22,29 +58,32 @@ class EPUB {
     return fs.statSync(this.path).isDirectory();
   }
 
-  extract() {
-    return new Promise((resolve, reject) => {
-      if (this.basedir !== undefined) {
-        resolve(this);
-      } else if (this.expanded) {
-        winston.verbose('EPUB is already unpacked');
-        this.basedir = this.path;
-        resolve(this);
-      } else {
-        winston.verbose('Extracting EPUB');
-        const tmpdir = tmp.dirSync({ unsafeCleanup: true }); // remove even when not empty
-        unzip(this.path, { dir: tmpdir.name }, (err) => {
-          if (err) {
-            winston.error('Failed to unzip EPUB (the ZIP archive may be corrupt).');
-            reject(err);
-          } else {
-            this.basedir = tmpdir.name;
-            resolve(this);
-          }
-        });
+  async extract() {
+    if (this.basedir !== undefined) {
+      return this;
+    } else if (this.expanded) {
+      winston.verbose('EPUB is already unpacked');
+      this.basedir = this.path;
+      return this;
+    } else {
+      winston.verbose('Extracting EPUB');
+      let unzippedDir;
+      try {
+        unzippedDir = await unzip(this.path);
+      } catch (error) {
+        winston.error('Failed to unzip EPUB (the ZIP archive may be corrupt).');
+        winston.debug(error);
+        try {
+          unzippedDir = await retryUnzip(this, error);
+        } catch (error) {
+          throw error;
+        }
       }
-    });
+      this.basedir = unzippedDir;
+      return this;
+    }
   }
+
 
   parse() {
     return new Promise((resolve, reject) => {
