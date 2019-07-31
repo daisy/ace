e9ca6e71a2e4d2bfd39d21015e5c5d9fa94bf98c
Noelia Ruiz Martínez <nrm1977@gmail.com>
2019-07-31 11:36:36 +0200

    Revert "Merge branch 'master' into esLocalization"
    - Try to fix unsuccessful check

--- packages/ace-report-axe/src/l10n/localize.js
+++ packages/ace-report-axe/src/l10n/localize.js
@@ -2,7 +2,6 @@ const { newLocalizer } = require('@daisy/ace-localize');
 
 const enJson = require("./locales/en.json");
 const frJson = require("./locales/fr.json");
-const pt_BRJson = require("./locales/pt_BR.json");
 
 export const localizer = newLocalizer({
     en: {
@@ -13,9 +12,5 @@ export const localizer = newLocalizer({
     fr: {
         name: "Français",
         translation: frJson,
-    },
-    pt_BR: {
-        name: "Português do Brasil",
-        translation: pt_BRJson,
     },
 });
