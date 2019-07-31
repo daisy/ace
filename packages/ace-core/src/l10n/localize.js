5a5726ae40e5861efe490d3aaeb03b30646415a0
Daniel Weck <daniel.weck@gmail.com>
2019-07-31 13:48:49 +0100

    feat: Spanish es localization (PR #261 PR #260)

--- packages/ace-core/src/l10n/localize.js
+++ packages/ace-core/src/l10n/localize.js
@@ -3,6 +3,7 @@ const { newLocalizer } = require('@daisy/ace-localize');
 const enJson = require("./locales/en.json");
 const frJson = require("./locales/fr.json");
 const pt_BRJson = require("./locales/pt_BR.json");
+const esJson = require("./locales/es.json");
 
 
 export const localizer = newLocalizer({
@@ -18,5 +19,9 @@ export const localizer = newLocalizer({
     pt_BR: {
         name: "Português do Brasil",
         translation: pt_BRJson,
+    },
+    es: {
+        name: "Español",
+        translation: esJson,
     },
 });
