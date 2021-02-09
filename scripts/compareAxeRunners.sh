396f83d2ae1de2b76ac7cb1e1766b9aa1ebe4cc8
Daniel Weck <daniel.weck@gmail.com>
2021-02-09 21:20:33 +0000

    launch Electron before Puppeteer in the compare/benchmark script

--- scripts/compareAxeRunners.sh
+++ scripts/compareAxeRunners.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 rm -rf ./CompareAxeRunners
-echo "PuppeteerAxeRunner ..."
-ACE_PERF=1 node ./packages/ace-cli/bin/ace.js -f -s -o ./CompareAxeRunners/PuppeteerAxeRunner "$1"
+
 echo "ElectronAxeRunner ..."
 ACE_PERF=1 node node_modules/electron/cli.js ./packages/ace-axe-runner-electron/lib/cli.js -f -s -o ./CompareAxeRunners/ElectronAxeRunner "$1"
+
+echo "PuppeteerAxeRunner ..."
+ACE_PERF=1 node ./packages/ace-cli/bin/ace.js -f -s -o ./CompareAxeRunners/PuppeteerAxeRunner "$1"
+
 echo "Report difference:"
 node node_modules/json-diff/bin/json-diff.js ./CompareAxeRunners/PuppeteerAxeRunner/report.json ./CompareAxeRunners/ElectronAxeRunner/report.json
