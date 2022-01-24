019c28d8a73567efcae88abc2847c9518a8565aa
Daniel Weck <daniel.weck@gmail.com>
2022-01-24 19:39:46 +0000

    added better compare script JSON report normalise

--- scripts/compareAxeRunners_VERBOSE.sh
+++ scripts/compareAxeRunners_VERBOSE.sh
@@ -11,5 +11,8 @@ ACE_PERF=1 node ./packages/ace/bin/ace-electron.js -f -V -o ./CompareAxeRunners/
 echo "PuppeteerAxeRunner ..."
 ACE_PERF=1 node ./packages/ace-cli/bin/ace.js -f -V -o ./CompareAxeRunners/PuppeteerAxeRunner "$1"
 
+node ./scripts/normalise_report_json.js ./CompareAxeRunners/PuppeteerAxeRunner/report.json
+node ./scripts/normalise_report_json.js ./CompareAxeRunners/ElectronAxeRunner/report.json
+
 echo "Report difference:"
-node node_modules/json-diff/bin/json-diff.js ./CompareAxeRunners/PuppeteerAxeRunner/report.json ./CompareAxeRunners/ElectronAxeRunner/report.json
+node node_modules/json-diff/bin/json-diff.js ./CompareAxeRunners/PuppeteerAxeRunner/report_normalised.json ./CompareAxeRunners/ElectronAxeRunner/report_normalised.json
