b85a2681ee24eb86818192191cad7e11504f24f4
Daniel Weck <daniel.weck@gmail.com>
2026-04-04 18:30:14 +0100

    chore(NPM): updated minor packages, checked with Taze and Socket Security Firewall

--- scripts/taze.sh
+++ scripts/taze.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-npx --no --offline --include-workspace-root --workspace . taze
+# rm -rf node_modules/ && rm yarn.lock && sfw yarn
+
+# npx --no --offline --include-workspace-root --workspace . taze
+# npx --no --offline --include-workspace-root --workspace $PWD taze
+# ===>
+# npm error code ERR_INVALID_ARG_TYPE
+# npm error The "paths[1]" argument must be of type string. Received undefined
+
+(npm exec --no --offline -- taze --fail-on-outdated --all --force --recursive --include-locked --concurrency 10 --loglevel debug --cwd . && npm exec --no --offline -- taze major --fail-on-outdated --all --force --recursive --include-locked --concurrency 10 --loglevel debug --cwd .) || echo OK
 
 # for relativefilepath in packages/*; do
 #     filename=`basename "${relativefilepath}"`
 #     echo "${filename} (${relativefilepath})"
-#     cd "${relativefilepath}"
-#     npx --no --offline --include-workspace-root --workspace . taze
-#     cd - &> /dev/null
+#     #cd "${relativefilepath}"
+#     npm exec --no --offline -- taze --force --cwd "${relativefilepath}" --all
+#     #cd - &> /dev/null
 # done
